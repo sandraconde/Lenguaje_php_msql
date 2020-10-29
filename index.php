2e08315ebc2f6186b77a0baeea03e01b5a6693bf
sandraconde <sandraconde@GitHub.com>
2020-10-29 16:54:05 -0500

    subiendo

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include 'conexion/index.php';
  <body>
 
  	<?php 
-
+/*
  	$var1 = 12;
  	echo $var1;
 
@@ -27,6 +27,70 @@ include 'conexion/index.php';
 
  	$var5 = $var4;
  	echo $var5;
+ 	*/
+ 	 
+ 	 /*
+ 	 if (1==2){ 
+ 	 	echo "uno";
+ 	 	}else{
+ 	 		echo "dos";
+ 		}*/
+ 	$var =1;
+ 	switch ($var) {
+ 			case '1':
+ 	 	# code...
+ 	 	 echo"enero";
+ 	 	 break;
+ 	 	 case '2':
+ 	 	# code...
+ 	 	 echo"febrero";
+ 	 	 break;
+ 	case '3':
+ 	 	# code...
+ 	 	 echo"marzo";
+ 	 	 break;
+ 	 	 case '4':
+ 	 	# code...
+ 	 	 echo"abril";
+ 	 	 break;
+ 	 	 case '5':
+ 	 	# code...
+ 	 	 echo"mayo";
+ 	 	 break;
+ 	case '6':
+ 	 	# code...
+ 	 	 echo"junio";
+ 			break;
+ 		case '7':
+ 	 	# code...
+ 	 	 echo"julio";
+ 			break;
+ 			case '8':
+ 	 	# code...
+ 	 	 echo"agosto";
+ 			break;
+ 			case '9':
+ 	 	# code...
+ 	 	 echo"septiembre";
+ 			break;
+ 			case '10':
+ 	 	# code...
+ 	 	 echo"octubre";
+ 			break;
+ 			case '11':
+ 	 	# code...
+ 	 	 echo"noviembre";
+ 			break;
+ 			case '12':
+ 	 	# code...
+ 	 	 echo"diciembre";
+ 			break;
+ 		default:
+ 			# code...
+ 			echo "hola";
+ 			break;
+ 	}
  	 ?>
+ 
  </body>
  </html>
