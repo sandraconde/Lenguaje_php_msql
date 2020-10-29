270c15e009064874b7b8c71d7da9b96278b23d8c
sandraconde <sandraconde@GitHub.com>
2020-10-29 15:43:37 -0500

    subiendo

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+include 'conexion/index.php';
  ?>
  <!DOCTYPE html>
  <html>
@@ -10,6 +11,11 @@
  </head>
  <body>
  	<?php 
+ 	$var1 = 12:
+ 	$var2 = "hola";
+ 	$var3 = 13.14;
+ 	$var4 = $var1.$var2;
+ 	$var5 = $var4;
  	 ?>
  </body>
  </html>
